644a6a3e63987cede618f2060c2fadea513639a4
gxywang <gxwang2@illinois.edu>
2025-02-05 23:38:45 -0600

    fixed home, about, meetings

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
+import logo from '../assets/logo/android-chrome-192x192.png'
 
 const Navbar: React.FC = () => {
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-logo">
+          <img src={logo} alt="sigrobotics logo"
+                style={{
+                  width: '64px',
+                  height: '64px',
+                  objectFit: 'cover',
+                  objectPosition: 'bottom',
+                }}/>
           SIGRobotics
         </Link>
         <ul className="navbar-menu">
-          <li className="navbar-item">
+          {/* <li className="navbar-item">
             <Link to="/" className="navbar-link">Home</Link>
-          </li>
+          </li> */}
           <li className="navbar-item">
             <Link to="/about" className="navbar-link">About</Link>
           </li>
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 import '../styles/style.css';
 import bhargav from '../assets/exec/bhargav.jpg';
 import gloria from '../assets/exec/gloria.jpg';
+import advait from '../assets/exec/advait.jpg'
 
 const About: React.FC = () => {
   return (
     <div className="layout-xl">
       <h1 className="heading-title">About</h1>
-      <br></br>
       <section>
         <h2 className="heading-subtitle-bold">Welcome to SIGRobotics!</h2>
         <p>We are an ACM@UIUC Special Interest Group for robotics dedicated to fostering an environment where students can learn, experiment, and build all kinds of robots.</p>
@@ -30,9 +30,10 @@ const About: React.FC = () => {
       <section>
         <h2 className="heading-subtitle-bold">Our Chairs</h2>
         <p></p>
-        <div style={{ display: 'flex', justifyContent: 'left', alignItems: 'center', gap: '20px', flexWrap: 'wrap' }}>
+        <div style={{ display: 'flex', justifyContent: 'left', alignItems: 'center', gap: '20px', flexWrap: 'wrap', marginTop: '2rem' }}>
           <div style={{textAlign: 'center'}}>
-            <img
+            <a href="https://bchandaka.github.io/" target="_blank" rel="noopener noreferrer">
+              <img
                 src={bhargav}
                 alt="team members"
                 style={{
@@ -42,12 +43,22 @@ const About: React.FC = () => {
                   borderRadius: '10px',
                   boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)'
                 }}
-            />
+                onMouseEnter={(e) => {
+                  e.currentTarget.style.transform = 'scale(1.03)';
+                  e.currentTarget.style.boxShadow = '0 8px 16px rgba(0, 0, 0, 0.2)';
+                }}
+                onMouseLeave={(e) => {
+                  e.currentTarget.style.transform = 'scale(1)';
+                  e.currentTarget.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.1)';
+                }}
+              />
+            </a>
             <h5 style={{marginTop: '1px'}}>Bhargav Chandaka</h5>
           </div>
 
           <div style={{textAlign: 'center'}}>
-            <img
+            <a href="https://gxywang.github.io/" target="_blank" rel="noopener noreferrer">
+              <img
                 src={gloria}
                 alt="team members"
                 style={{
@@ -57,8 +68,42 @@ const About: React.FC = () => {
                   borderRadius: '10px',
                   boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)'
                 }}
-            />
+                onMouseEnter={(e) => {
+                  e.currentTarget.style.transform = 'scale(1.03)';
+                  e.currentTarget.style.boxShadow = '0 8px 16px rgba(0, 0, 0, 0.2)';
+                }}
+                onMouseLeave={(e) => {
+                  e.currentTarget.style.transform = 'scale(1)';
+                  e.currentTarget.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.1)';
+                }}
+              />
+            </a>
             <h5 style={{marginTop: '1px'}}>Gloria Wang</h5>
+          </div>
+
+          <div style={{textAlign: 'center'}}>
+            <a href="https://advaitpatel.com" target="_blank" rel="noopener noreferrer">
+              <img
+                src={advait}
+                alt="team members"
+                style={{
+                  width: '200px',
+                  height: '200px',
+                  objectFit: 'cover',
+                  borderRadius: '10px',
+                  boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)'
+                }}
+                onMouseEnter={(e) => {
+                  e.currentTarget.style.transform = 'scale(1.03)';
+                  e.currentTarget.style.boxShadow = '0 8px 16px rgba(0, 0, 0, 0.2)';
+                }}
+                onMouseLeave={(e) => {
+                  e.currentTarget.style.transform = 'scale(1)';
+                  e.currentTarget.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.1)';
+                }}
+              />
+            </a>
+            <h5 style={{marginTop: '1px'}}>Advait Patel</h5>
           </div>
         </div>
       </section>
